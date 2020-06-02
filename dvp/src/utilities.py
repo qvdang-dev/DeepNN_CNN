839caa20f4bcff8762969849d5e5a4bc8b6b9344
qvdang-dev <qvdang.96@gmail.com>
2020-06-03 01:12:22 +0700

    modify command for choosing box size when sliding

--- dvp/src/utilities.py
+++ dvp/src/utilities.py
@@ -7,12 +7,14 @@ def command_parsing():
     # construct the argument parser and parse the arguments
     ap = argparse.ArgumentParser()
     ap.add_argument("-i", "--image", required=True, help="Path to the image")
+    ap.add_argument("-bw", "--boxwidth", type=int ,help="width of sliding box")
+    ap.add_argument("-bh", "--boxhight", type=int ,help="hight of sliding box")
     
     args = vars(ap.parse_args())
     # load the image and define the window width and height
     image = cv2.imread(args["image"])
-    (boxWidth, boxHight) = (28, 28)
-    
+    (boxWidth, boxHight) = (args["boxwidth"], args["boxhight"])
+
     return image, boxWidth, boxHight
 
 def sliding_image(image, w, h):
